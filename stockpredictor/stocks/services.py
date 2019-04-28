d52bf382a031fb9694631e9ac1d5a6046a40f493
joshjanda1 <joshjanda1@users.noreply.github.com>
2019-04-28 12:00:42 -0500

    Clean & Edit
    Edited details page, changed data source to IEX since the API limit is much higher. Cleaned up the services file, added some extra variables in views function that are relevant to stock data.

--- stockpredictor/stocks/services.py
+++ stockpredictor/stocks/services.py
@@ -1,41 +1,23 @@
-import matplotlib.pyplot as plt
-from pandas.plotting import scatter_matrix
 import pandas as pd
 import numpy as np
-import alpha_vantage
-from alpha_vantage.timeseries import TimeSeries
-from alpha_vantage.techindicators import TechIndicators
+from iexfinance.stocks import Stock, get_historical_data
 
-
-def get_stock(symbol='SPX'):
+def get_stock(symbol):
 	"""
 		This function gets stock information of required symbol. Default is SP500.
-		Info is obtained from Alpha Vantage API
+		Info is obtained from IEX Finance
 		- Symbol -> Stock Symbol (e.g SPX)
 	"""
+	stock = Stock(symbol)
+	stock_data = get_historical_data(symbol, output_format='pandas')#gets full historical data on stock
 	
-	#get tech indicators for stock
-	ti = TechIndicators(key= '5FWAPV1GCOE2WQLV', output_format = 'pandas')
-		
-	sma, _ = ti.get_sma(symbol=symbol, interval = 'daily')
-	macd, _ = ti.get_macd(symbol=symbol, interval = 'daily')
-	rsi, _ = ti.get_rsi(symbol=symbol, interval = 'daily')
-	adx, _ = ti.get_adx(symbol=symbol, interval = 'daily')
-
-	ts = TimeSeries(key = '5FWAPV1GCOE2WQLV', output_format = 'pandas')
-	data, _ = ts.get_daily(symbol=symbol, outputsize = 'full')
-		
-	intermediate_data = pd.concat([data, sma, macd, rsi, adx], axis=1, sort=True)
+	stock_data = stock_data.reset_index()#resets index so index is standard integers
+	dates = stock_data['date']#gets dates
+	stock_data = stock_data.drop('date', axis=1)
+	company = stock.get_company_name()
 	
-	data = intermediate_data.reset_index()#reset index so standard..
-	data['Open'], data['High'], data['Low'], data['Volume'], data['Close'] = data['1. open'], data['2. high'], data['3. low'], data['5. volume'], data['4. close']
-	data = data.drop(['1. open', '2. high', '3. low', '4. close', '5. volume'], axis=1)#gets conventional names for labels
-	#now remove null values for first month...
-	data = data[39:]
-	time = data['index'] #allows us to know time of data while reseting index to integers for easier use
-	data = data.drop('index', axis=1)
-	data = data.reset_index(drop = True) #resets index one more time since we dropped initial values in data since null
-	final_data = data
-	return final_data, time
+	return stock_data, dates, company
+	
+	
 		
     
--- stockpredictor/stocks/views.py
+++ stockpredictor/stocks/views.py
@@ -8,6 +8,7 @@ from bokeh.embed import components
 from bokeh.plotting import figure
 from bokeh.io import output_file, show, output_notebook
 from bokeh.resources import CDN
+from bokeh.models import HoverTool, ColumnDataSource
 import numpy as np
 import pandas as pd
 
@@ -23,27 +24,42 @@ def detail_view(request):
 	symbol = request.GET.get('usr_search')
 	template = loader.get_template('stocks/detail.html')
 	
-	get_stock_info_api, time = services.get_stock(symbol)
-	datelist = pd.DatetimeIndex(time)
+	get_stock_info_api, dates, company = services.get_stock(symbol)#gets stock info
 	
-	volatility_data = get_stock_info_api['Close'].iloc[-10: -1]#10 period volatility calculation
+	volatility_data = get_stock_info_api['close'].iloc[-10: -1]#10 period volatility calculation
 	volatility = np.std(volatility_data)
+	volatility = round(volatility, 4)
+	
+	one_day_percent_change = (np.log(get_stock_info_api['close'].iloc[-1])-np.log(get_stock_info_api['close'].iloc[-2]))*100
+	one_day_percent_change = round(one_day_percent_change, 2)#percentage change calculation
+	
+	close = get_stock_info_api['close']
+	dates_close = pd.concat([close, dates], axis=1, sort=True)#create dataframe of close and dates
+	dates_close_cds = ColumnDataSource(dates_close)#get data source for bokeh plot ##allows us to add tooltip
+	
 	
 	if get_stock_info_api is None:
 		raise Http404("Stock {0} does not exist, try again.".format(symbol))
+	hover = HoverTool(tooltips=[('Date', '@date{%F}'), ('Close', '$@{close}{%0.2f}'),],
+		formatters={'date': 'datetime', 'close': 'printf'})
 		
-	graph = figure(plot_width=800, plot_height=250, x_axis_type = 'datetime')
-	graph.line(datelist, get_stock_info_api['Close'], color='green', alpha=.5)
+	graph = figure(plot_width=800, plot_height=250, x_axis_type = 'datetime', 
+		title='Closing Data of {0}'.format(symbol))
+	graph.line("date", "close", color='green', alpha=.5, source=dates_close_cds)
+	graph.add_tools(hover)
+	graph.title.align = 'center'
 	script, div = components(graph)
 	
 	stock_info = {
 		'symbol': symbol,
-		'today_close': get_stock_info_api['Close'].iloc[-1],#gets todays closing price
-		'today_open': get_stock_info_api['Open'].iloc[-1],#gets todays open price
-		'today_high': get_stock_info_api['High'].iloc[-1],#gets todays high
-		'today_low': get_stock_info_api['Low'].iloc[-1],#gets todays low
-		'today_volume': get_stock_info_api['Volume'].iloc[-1],
+		'today_close': get_stock_info_api['close'].iloc[-1],#gets todays closing price
+		'today_open': get_stock_info_api['open'].iloc[-1],#gets todays open price
+		'today_high': get_stock_info_api['high'].iloc[-1],#gets todays high
+		'today_low': get_stock_info_api['low'].iloc[-1],#gets todays low
+		'today_volume': get_stock_info_api['volume'].iloc[-1],
 		'current_volatility': volatility,
+		'one_day_percent_change': one_day_percent_change,
+		'company': company,
 		'script': script,
 		'div': div,
 	}
